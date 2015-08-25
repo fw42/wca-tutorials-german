d6237a1da3bc1e0f9708f7764a62258f4384b94f
Laura Ohrndorf <laura.ohrndorf@upb.de>
2015-08-26 01:45:54 +0200

    Aktualisierte Tutorials

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <br/>
 <hr/>
 <p align="center" style="font-size: 95%">
-<a href="competitor.php"><b>Teil 1:</b> Competitor Tutorial</a>&nbsp;&mdash;&nbsp;<a href="solve.php"><b>Teil 2:</b> Puzzle Lösen</a>&nbsp;&mdash;&nbsp;<a href="judging.php"><b>Teil 3:</b> Judging Tutorial</a>
+<a href="competitor.php"><b>Teil 1:</b> Competitor Tutorial</a>&nbsp;&mdash;&nbsp;<a href="judging.php"><b>Teil 2:</b> Judging Tutorial</a>
 </p>
 <p align="center"><a href="competition.php">Competition Orga Tutorial</a></p>
 <hr/>
