8c055cf91fe90cb6fc72ee8efc7f42b7d465d6b0
Laura Ohrndorf <laura.ohrndorf@upb.de>
2015-08-26 01:17:05 +0200

    Tutorials komplett überarbeitet und zusammengefasst

--- competitor.php
+++ competitor.php
@@ -8,175 +8,61 @@ Anleitung für Teilnehmer eines WCA Wettbewerbs
 
 <p align="center" class="small">
 Dieser Text basiert auf dem
-<a href="http://www.cubingusa.com/ctutorial.php">Competitor Tutorial</a>
+<a href="http://www.cubingusa.com/jtutorial.php">Judging Tutorial</a>
 von <a href="http://www.cubingusa.com">Cubing USA</a>. <br/>
 Übersetzt und ergänzt von <a href="http://hackvalue.de">Florian Weingarten</a>
-(<a href="http://www.worldcubeassociation.org/results/p.php?i=2007WEIN01">2007WEIN01</a>)<br/>
-<a href="http://aachen.speedcubing.com/">
-http://aachen.speedcubing.com/</a>
+(<a href="http://www.worldcubeassociation.org/results/p.php?i=2007WEIN01">2007WEIN01</a>) und vielen anderen.
 </p>
-
-<? include "credits.php" ?>
 
 <? include "menu.php" ?>
 
 <p>
-Du möchtest an deinem ersten Speedcubing-Wettbewerb ("Competition")
-teilnehmen, aber weißt nicht wie es dort so abläuft? Diese Seite
-soll dir eine kleine Hilfe sein, damit deine erste Competition
-ein schönes Erlebnis wird. Jede Competition ist anders, aber ein
-paar Grundabläufe sind bei fast allen Competitions gleich.
+Du möchtest an deinem ersten Speedcubing-Wettbewerb ("Competition") teilnehmen, aber weißt nicht wie es dort so abläuft? Diese Seite soll dir eine kleine Hilfe sein, damit deine erste Competition ein schönes Erlebnis wird. Jede Competition ist anders, aber die Grundabläufe sind bei allen Competitions gleich, da sie stets nach dem WCA Regelwerk ablaufen. Die WCA ist eine internationale Organisation für das Speedcubing, etwa wie die FIFA für den Fußball. Auf jedem Turnier ist mindestens ein offizieller WCA Delegierter anwesend, der die Einhaltung der Regeln überwacht und stets gerufen werden sollte, wenn jemand nicht genau weiß, was zu tun ist.
 </p>
 
-<h2>Kenne die WCA Regeln</h2>
+<p>Achtung! Dies nur eine vereinfachte Zusammenfassung für Neulinge. Vor der Teilnahme an einem Turnier sollte jeder Teilnehmer die vollständigen WCA Regulations gelesen haben. Eine deutsche Übersetzung gibt es hier: <a href="https://www.worldcubeassociation.org/regulations/translations/german/">https://www.worldcubeassociation.org/regulations/translations/german/</a>.</p>
 
+<h2>Informiere dich vorher!</h2>
 
-<p>
-Die
-<a href="http://www.worldcubeassociation.org/">World Cube Association (WCA)</a>
-ist die regulierende Instanz im Speedcubing, wie etwa der DFB beim Fußball
-oder die NBA beim Basketball. Die WCA verwaltet die Ergebnisse der
-Competitions, stellt die internationalen und nationalen Ranglisten zur
-Verfügung und stellt die Regeln für den Ablauf einer Competition auf.
-Diese Regeln solltest du daher kennen um nicht Gefahr zu laufen
-disqualifiziert zu werden, weil du z.B. den Timer nicht richtig startest
-oder zur richtigen Zeit nicht am richtigen Ort bist. Du musst die
-Regeln natürlich nicht komplett auswendig kennen, aber du solltest unbedingt
-einmal aufmerksam die gesamten Regeln lesen, bevor du zu deinem ersten
-Wettkampf gehst. Falls du
-Fragen hast oder einzelne Teile der Regeln nicht verstehst, so solltest
-du unbedingt den Veranstalter oder andere Cuber vor der Competition
-oder während der Competition (aber auf jeden Fall bevor du dran bist)
-um Hilfe bitten. Die offiziellen WCA Regeln findest du hier:
-<a href="http://www.worldcubeassociation.org/regulations/">
-http://www.worldcubeassociation.org/regulations/</a>.
-Eine deutsche Übersetzung gibt es hier:
-<a href="https://www.worldcubeassociation.org/regulations/translations/german/">
-https://www.worldcubeassociation.org/regulations/translations/german/</a>.
-</p>
+<p>Bevor du auf ein Turnier gehst, lies dir die Webseite des Turniers aufmerksam durch. Diese enthält wichtige Informationen, z.B. den Zeitplan, Zeitlimits, die du erreichen musst und vieles mehr. In der Regel verschicken Turnierorganisatoren kurz vor einem Turnier Rundmails. Auch diese solltest du aufmerksam lesen.</p>
 
-<h2>Vorher anmelden!</h2>
+<h2>Puzzle</h2>
 
 <p>
-Alle Wettbewerbe müssen über eine Webseite verfügen auf der wichtige
-Informationen angekündigt werden. Eine Liste aller bereits angekündigten
-Competitions findest du hier:
-<a href="http://www.worldcubeassociation.org/results/competitions.php">
-http://www.worldcubeassociation.org/results/competitions.php</a>.
-Dort sind auch die Webseiten, soweit vorhanden, eingetragen. Jede Webseite
-enthält auch immer ein Anmeldeformular, mit dem du dich anmelden musst. 
-Die Organisatoren werden dankbar sein, wenn sie schon lange vorher bereits 
-wissen, mit wie vielen Personen
-sie rechnen müssen. Melde dich daher so früh wie möglich an.
-<b>Sehr wichtig:</b>
-Falls du doch keine Zeit oder Lust hast, melde dich rechtzeitig wieder ab!
-Bedenke, dass die Organisatoren die Eintrittsgelder meist so planen, dass
-sie durch die angemeldeten Teilnehmer die Organisationskosten wieder
-hereinbekommen. Bleiben also zu viele angemeldete Teilnehmer fern, so
-machen die Organisatoren Verluste. Außerdem kann es sein, dass es Wartelisten gibt 
-und du einem anderen Teilnehmer die Chance verwehrst teilzunehmen, wenn du dich nicht 
-abmeldest. Wenn du dich nur für ein einzelnes Event nach- oder abmelden möchtest,
-melde dich per E-Mail beim Organisator.
-</p>
-
-<h2>Training</h2>
-
-<p>
-Du beherrschst deine Methode gut und schaffst zuhause Zeiten mit denen
-du zufrieden bist? Auf einer Competition sieht das meistens anders aus.
-Du bist nervös, deine Hände zittern und dir gucken unter Umständen
-50 Leute oder mehr ganz genau mit kritischen Blicken auf die Finger.
-Um keine bösen Überraschungen zu erleben, solltest du zuhause üben vor
-vielen Leute zu lösen, z.B. vor Freunden oder der Familie.
-Auch Cuben in der Öffentlichkeit, z.B. in der Schule, in der Stadt,
-im Bus oder in der Bahn sind ein gutes Training um Nervosität zu vermeiden. 
-</p>
-
-<h2>Zustand deiner Puzzle</h2>
-
-<p>
-Sind die Sticker auf deinem Cube sehr abgenutzt, so solltest du
-in Betracht ziehen diese gegen Neue zu ersetzen (z.B. von
-<a href="http://www.cubikon.de/">Cubikon</a> oder 
-<a href="http://www.cubesmith.com/">Cubesmith</a>).
-Besonders beim Blindlösen kann es sonst unter Umständen passieren,
-dass du mit deinem Puzzle nicht teilnehmen darfst.
+Auf WCA Turnieren sind die meisten Puzzle erlaubt (siehe Abschnitt 3 der WCA Regeln). Wichtig ist, dass deine Puzzle sich in einem guten Zustand befinden. Sind die Sticker auf deinem Cube also sehr abgenutzt, so solltest du überlegen, diese gegen neue zu ersetzen. Besonders beim Blindlösen kann es sonst passieren, dass du mit deinem Puzzle nicht teilnehmen darfst.
 </p>
 
 <h2>Plane deinen Tag</h2>
 
 <p>
-Die meisten Competitions stellen im Voraus einen Zeitplan auf ihrer
-Webseite zur Verfügung. Schaue dir an, wann die Events, für die du angemeldet
-bist, stattfinden und sei pünktlich zum Anfang jedes Events in der Nähe.
-Wer aufgerufen wird, aber gerade draußen eine Zigarette raucht oder auf dem
-Klo sitzt, kann schnell disqualifiziert werden.
+Der Zeitplan eines Turniers ist vorab auf dessen Webseite verfügbar. Schaue dir an, wann die Events, für die du angemeldet bist, stattfinden und sei pünktlich zum Anfang jedes dieser Events bereit teilzunehmen.
 </p>
 
 <h2>Warte bis dein Name aufgerufen wird</h2>
 
 <p>
-Teilnehmer werden normalerweise in Gruppen, meist zwei, eingeteilt.
-Nur weil du für ein Event angemeldet bist, heißt das daher noch
-nicht unbedingt, dass du auch
-sofort an der Reihe bist. Am Anfang des Events werden in der Regel die Leute
-aufgerufen, die zur momentanen Gruppe gehören. Achte darauf, ob dein
-Name dabei ist. Zusätzlich hängen auf den meisten Competitions auch Listen aus, 
-auf denen man nachschauen kann, in welche Gruppe man jeweils eingeteilt ist.
-Falls du aufgerufen wirst, solltest du dein Puzzle zum Mischen abgeben
-und dich anschließend in den Wartebereich begeben. Falls dein Name nicht
-aufgerufen wird, bist du in einer der nächsten Gruppen.
-Von allen Teilnehmern,
-die für ein Event angemeldet sind, aber die nicht in der aktuellen Gruppe
-sind, wird erwartet, dass sie als Schiedsrichter oder als Scrambler
-zur Verfügung stehen (wer sich weigert, kann disqualifiziert werden!).
-Wenn du nicht weißt, was ein Schiedsrichter zu tun hat, solltest du unbedingt
-das <a href="judging.php">Judging Tutorial</a> lesen.
-Sollte dein Name auch in der letzten Gruppe nicht aufgerufen werden,
-so melde dich umgehend beim Scrambletisch. Auch Organisatoren sind
-nur Menschen, und so kann es unter Umständen schon mal passieren, dass
-dein Teilnehmerzettel bei einem Event vergessen wurde oder verloren
-gegangen ist.
-</p>
+Teilnehmer eines Events werden meistens in Gruppen eingeteilt, die du den ausgehängten Listen entnehmen kannst. Sobald dein Name oder deine Gruppe aufgerufen wird, gehe zum Scrambling Tisch und lege dein Puzzle auf den Zettel (Scoresheet), auf dem dein Name steht. Danach setzt du dich in den Wartebereich. Wenig später wird dich ein Judge aufrufen. Gehe zusammen mit ihm zu einem der Timertische und setze dich. Der Judge legt das Puzzle vor dir auf die Matte (Stackmat). Zu Beginn des Versuchs ist es mit einer Abdeckung verdeckt. Dein Judge wird dich nun fragen, ob du bereit bist ("Ready?"). Falls du zu diesem Zeitpunkt noch unsicher bist, was den weiteren Ablauf angeht (z.B. wenn du nicht weißt, wie der Timer funktioniert), zögere nicht den Schiedsrichter einfach zu fragen. Sobald du aber eindeutig bejahst ("Ja!"), dass du bereit bist, wird er die Abdeckung hochheben, womit die Inspection Phase beginnt.</p>
 
-<h2>Du wurdest aufgerufen</h2>
+<h2>Inspection und Beginn des Versuchs</h2>
 
 <p>
-Jetzt geht es los. Dein Name wurde aufgerufen. Es steht ein
-Schiedsrichter mit deinem gemischten Puzzle beim Wartebereich um
-dich abzuholen. Gehe zu ihm und lasse dir weitere Anweisungen geben.
-Wenn nötig, sage ihm, dass das deine erste Competition ist und dass er dir bitte
-noch einmal erklären soll, wie es jetzt genau weiter geht.
-Mehr findest du im <a href="solve.php">Puzzle Lösen Tutorial</a>.
+Du hast nun bis zu 15 Sekunden Zeit, dir das Puzzle anzuschauen und den Anfang deiner Lösung zu planen. In dieser Zeit passiert folgendes: Du darfst das Puzzle in der Hand halten, aber keine Drehungen machen. Dein Judge wird dir nach 8 Sekunden ("8 seconds") und 12 Sekunden ("12 seconds") die Zeit ansagen. Du kannst allerdings jederzeit in der Inspection Phase den Versuch starten. Dazu legst du das Puzzle wieder auf die Matte und startest den Timer, indem du beide Hände <i>flach</i> auf die Sensoren legst. Warte kurz bis die grüne Lampe leuchtet. Wenn du nun die Hände herunternimmst, läuft die Zeit und du kannst anfangen das Puzzle zu lösen. Spätestens nach den 15 Sekunden musst du den Timer gestartet haben.
 </p>
 
-<h2>Fertig? Hinsetzen!</h2>
+<h2>Ende des Versuchs</h2>
 
 <p>
-Normalerweise löst jeder Teilnehmer ein Puzzle fünf mal (drei mal für
-bestimmte Puzzle). Nachdem du dein Puzzle gelöst hast, musst du zurück
-in den Wartebereich. Nimm dein Puzzle nicht mit, sondern gib es dem
-Schiedsrichter, damit dieser es erneut mischen lassen kann.
+Wenn du das Puzzle gelöst hast, lege das Puzzle auf den Tisch und stoppe den Timer. Dazu musst du wieder die Hände <i>flach</i> auf die Sensorflächen legen. Achte darauf, dass du das Puzzle dabei nicht mehr berührst.
+
+Dein Judge wird nun überprüfen, ob das Puzzle gelöst ist und deine Zeit aufschreiben. Er wird dir auch sagen, falls es Regelverstösse gab. Warte dies ab und berühre währenddessen nicht das Puzzle. Anschliessend musst du auf dem Scoresheet unterschreiben um das Resultat zu bestätigen. Bevor du dies tust, vergewissere dich dass die Zeit richtig aufgeschrieben wurde. Wenn du unsicher bist, zögere auch hier nicht nachzufragen oder sogar den WCA Delegierten herbeizurufen.
+
+Wenn du noch weitere Versuche machen darfst, wird der Judge das Puzzle mitnehmen und zurück zum Scrambling Tisch bringen. Du musst dich dann zurück in den Wartebereich setzen. Ansonsten ist das Event für dich vorbei und du kannst dein Puzzle wieder mitnehmen.
 </p>
 
-<p>
-In einem "kombinierten Finale" kann es sein, dass du nicht alle fünf
-Versuche machen darfst, sondern nur einen oder zwei. Anhand deiner
-besten Zeit wird dann entschieden, ob du alle fünf machen darfst.
-Falls nicht, bist du fertig und darfst den Wartebereich verlassen und
-auf das nächste Event warten.
-</p>
-
-<h2>Hab Spaß!</h2>
+<h2>Judging</h2>
 
 <p>
-Bei Competitions geht es nicht nur um gute Zeiten. Es geht um Spaß. Wer
-viel Spaß hat und locker drauf ist, wird gleichzeitig bessere Zeiten
-machen.
-</p>
-
-<p>
-Weiter zu <a href="solve.php"><b>Teil 2:</b> Puzzle Lösen</a>.
+Dieser erste Abschnitt des Tutorials diente dazu, dich auf ein Turnier als Teilnehmer vorzubereiten. Als Teilnehmer eines WCA Turniers ist man allerdings auch dazu verpflichtet, als Schiedsrichter (Judge) mitzuhelfen, wenn man selber gerade nicht teilnimmt. Schau dir hierzu  <a href="judging.php"><b>Teil 2:</b> Judging Tutorial</a> an.
 </p>
 
 <? include "footer.php" ?>
