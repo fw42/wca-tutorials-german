4301d9c4ecb0d8efe06d24e3d3703c8402563962
Laura Ohrndorf <laura.ohrndorf@upb.de>
2015-08-26 02:11:07 +0200

    + Linebreaks

--- competitor.php
+++ competitor.php
@@ -17,52 +17,95 @@ von <a href="http://www.cubingusa.com">Cubing USA</a>. <br/>
 <? include "menu.php" ?>
 
 <p>
-Du möchtest an deinem ersten Speedcubing-Wettbewerb ("Competition") teilnehmen, aber weißt nicht wie es dort so abläuft? Diese Seite soll dir eine kleine Hilfe sein, damit deine erste Competition ein schönes Erlebnis wird. Jede Competition ist anders, aber die Grundabläufe sind bei allen Competitions gleich, da sie stets nach dem WCA Regelwerk ablaufen. Die WCA ist eine internationale Organisation für das Speedcubing, etwa wie die FIFA für den Fußball. Auf jedem Turnier ist mindestens ein offizieller WCA Delegierter anwesend, der die Einhaltung der Regeln überwacht und stets gerufen werden sollte, wenn jemand nicht genau weiß, was zu tun ist.
+Du möchtest an deinem ersten Speedcubing-Wettbewerb ("Competition") teilnehmen, aber weißt
+nicht wie es dort so abläuft? Diese Seite soll dir eine kleine Hilfe sein, damit deine erste
+Competition ein schönes Erlebnis wird. Jede Competition ist anders, aber die Grundabläufe
+sind bei allen Competitions gleich, da sie stets nach dem WCA Regelwerk ablaufen. Die WCA
+ist eine internationale Organisation für das Speedcubing, etwa wie die FIFA für den Fußball.
+Auf jedem Turnier ist mindestens ein offizieller WCA Delegierter anwesend, der die Einhaltung
+der Regeln überwacht und stets gerufen werden sollte, wenn jemand nicht genau weiß, was zu tun ist.
 </p>
 
-<p>Achtung! Dies nur eine vereinfachte Zusammenfassung für Neulinge. Vor der Teilnahme an einem Turnier sollte jeder Teilnehmer die vollständigen WCA Regulations gelesen haben. Eine deutsche Übersetzung gibt es hier: <a href="https://www.worldcubeassociation.org/regulations/translations/german/">https://www.worldcubeassociation.org/regulations/translations/german/</a>.</p>
+<p>Achtung! Dies nur eine vereinfachte Zusammenfassung für Neulinge. Vor der Teilnahme an einem
+Turnier sollte jeder Teilnehmer die vollständigen WCA Regulations gelesen haben. Eine deutsche
+Übersetzung gibt es hier: <a href="https://www.worldcubeassociation.org/regulations/translations/german/">https://www.worldcubeassociation.org/regulations/translations/german/</a>.</p>
 
 <h2>Informiere dich vorher!</h2>
 
-<p>Bevor du auf ein Turnier gehst, lies dir die Webseite des Turniers aufmerksam durch. Diese enthält wichtige Informationen, z.B. den Zeitplan, Zeitlimits, die du erreichen musst und vieles mehr. In der Regel verschicken Turnierorganisatoren kurz vor einem Turnier Rundmails. Auch diese solltest du aufmerksam lesen.</p>
+<p>Bevor du auf ein Turnier gehst, lies dir die Webseite des Turniers aufmerksam durch. Diese
+enthält wichtige Informationen, z.B. den Zeitplan, Zeitlimits, die du erreichen musst und
+vieles mehr. In der Regel verschicken Turnierorganisatoren kurz vor einem Turnier Rundmails.
+Auch diese solltest du aufmerksam lesen.</p>
 
 <h2>Puzzle</h2>
 
 <p>
-Auf WCA Turnieren sind die meisten Puzzle erlaubt (siehe Abschnitt 3 der WCA Regeln). Wichtig ist, dass deine Puzzle sich in einem guten Zustand befinden. Sind die Sticker auf deinem Cube also sehr abgenutzt, so solltest du überlegen, diese gegen neue zu ersetzen. Besonders beim Blindlösen kann es sonst passieren, dass du mit deinem Puzzle nicht teilnehmen darfst.
+Auf WCA Turnieren sind die meisten Puzzle erlaubt (siehe Abschnitt 3 der WCA Regeln). Wichtig ist,
+dass deine Puzzle sich in einem guten Zustand befinden. Sind die Sticker auf deinem Cube also sehr
+abgenutzt, so solltest du überlegen, diese gegen neue zu ersetzen. Besonders beim Blindlösen kann
+es sonst passieren, dass du mit deinem Puzzle nicht teilnehmen darfst.
 </p>
 
 <h2>Plane deinen Tag</h2>
 
 <p>
-Der Zeitplan eines Turniers ist vorab auf dessen Webseite verfügbar. Schaue dir an, wann die Events, für die du angemeldet bist, stattfinden und sei pünktlich zum Anfang jedes dieser Events bereit teilzunehmen.
+Der Zeitplan eines Turniers ist vorab auf dessen Webseite verfügbar. Schaue dir an, wann die
+Events, für die du angemeldet bist, stattfinden und sei pünktlich zum Anfang jedes dieser Events
+bereit teilzunehmen.
 </p>
 
 <h2>Warte bis dein Name aufgerufen wird</h2>
 
 <p>
-Teilnehmer eines Events werden meistens in Gruppen eingeteilt, die du den ausgehängten Listen entnehmen kannst. Sobald dein Name oder deine Gruppe aufgerufen wird, gehe zum Scrambling Tisch und lege dein Puzzle auf den Zettel (Scoresheet), auf dem dein Name steht. Danach setzt du dich in den Wartebereich. Wenig später wird dich ein Judge aufrufen. Gehe zusammen mit ihm zu einem der Timertische und setze dich. Der Judge legt das Puzzle vor dir auf die Matte (Stackmat). Zu Beginn des Versuchs ist es mit einer Abdeckung verdeckt. Dein Judge wird dich nun fragen, ob du bereit bist ("Ready?"). Falls du zu diesem Zeitpunkt noch unsicher bist, was den weiteren Ablauf angeht (z.B. wenn du nicht weißt, wie der Timer funktioniert), zögere nicht den Schiedsrichter einfach zu fragen. Sobald du aber eindeutig bejahst ("Ja!"), dass du bereit bist, wird er die Abdeckung hochheben, womit die Inspection Phase beginnt.</p>
+Teilnehmer eines Events werden meistens in Gruppen eingeteilt, die du den ausgehängten Listen
+entnehmen kannst. Sobald dein Name oder deine Gruppe aufgerufen wird, gehe zum Scrambling
+Tisch und lege dein Puzzle auf den Zettel (Scoresheet), auf dem dein Name steht. Danach setzt
+du dich in den Wartebereich. Wenig später wird dich ein Judge aufrufen. Gehe zusammen mit ihm
+zu einem der Timertische und setze dich. Der Judge legt das Puzzle vor dir auf die Matte
+(Stackmat). Zu Beginn des Versuchs ist es mit einer Abdeckung verdeckt. Dein Judge wird dich
+nun fragen, ob du bereit bist ("Ready?"). Falls du zu diesem Zeitpunkt noch unsicher bist, was
+den weiteren Ablauf angeht (z.B. wenn du nicht weißt, wie der Timer funktioniert), zögere nicht
+den Schiedsrichter einfach zu fragen. Sobald du aber eindeutig bejahst ("Ja!"), dass du bereit
+bist, wird er die Abdeckung hochheben, womit die Inspection Phase beginnt.</p>
 
 <h2>Inspection und Beginn des Versuchs</h2>
 
 <p>
-Du hast nun bis zu 15 Sekunden Zeit, dir das Puzzle anzuschauen und den Anfang deiner Lösung zu planen. In dieser Zeit passiert folgendes: Du darfst das Puzzle in der Hand halten, aber keine Drehungen machen. Dein Judge wird dir nach 8 Sekunden ("8 seconds") und 12 Sekunden ("12 seconds") die Zeit ansagen. Du kannst allerdings jederzeit in der Inspection Phase den Versuch starten. Dazu legst du das Puzzle wieder auf die Matte und startest den Timer, indem du beide Hände <i>flach</i> auf die Sensoren legst. Warte kurz bis die grüne Lampe leuchtet. Wenn du nun die Hände herunternimmst, läuft die Zeit und du kannst anfangen das Puzzle zu lösen. Spätestens nach den 15 Sekunden musst du den Timer gestartet haben.
+Du hast nun bis zu 15 Sekunden Zeit, dir das Puzzle anzuschauen und den Anfang deiner Lösung
+zu planen. In dieser Zeit passiert folgendes: Du darfst das Puzzle in der Hand halten, aber
+keine Drehungen machen. Dein Judge wird dir nach 8 Sekunden ("8 seconds") und
+12 Sekunden ("12 seconds") die Zeit ansagen. Du kannst allerdings jederzeit in der Inspection
+Phase den Versuch starten. Dazu legst du das Puzzle wieder auf die Matte und startest den Timer,
+indem du beide Hände <i>flach</i> auf die Sensoren legst. Warte kurz bis die grüne Lampe
+leuchtet. Wenn du nun die Hände herunternimmst, läuft die Zeit und du kannst anfangen das Puzzle
+zu lösen. Spätestens nach den 15 Sekunden musst du den Timer gestartet haben.
 </p>
 
 <h2>Ende des Versuchs</h2>
 
 <p>
-Wenn du das Puzzle gelöst hast, lege das Puzzle auf den Tisch und stoppe den Timer. Dazu musst du wieder die Hände <i>flach</i> auf die Sensorflächen legen. Achte darauf, dass du das Puzzle dabei nicht mehr berührst.
+Wenn du das Puzzle gelöst hast, lege das Puzzle auf den Tisch und stoppe den Timer. Dazu musst du
+wieder die Hände <i>flach</i> auf die Sensorflächen legen. Achte darauf, dass du das Puzzle dabei
+nicht mehr berührst.
 
-Dein Judge wird nun überprüfen, ob das Puzzle gelöst ist und deine Zeit aufschreiben. Er wird dir auch sagen, falls es Regelverstösse gab. Warte dies ab und berühre währenddessen nicht das Puzzle. Anschliessend musst du auf dem Scoresheet unterschreiben um das Resultat zu bestätigen. Bevor du dies tust, vergewissere dich dass die Zeit richtig aufgeschrieben wurde. Wenn du unsicher bist, zögere auch hier nicht nachzufragen oder sogar den WCA Delegierten herbeizurufen.
+Dein Judge wird nun überprüfen, ob das Puzzle gelöst ist und deine Zeit aufschreiben. Er wird dir
+auch sagen, falls es Regelverstösse gab. Warte dies ab und berühre währenddessen nicht das Puzzle.
+Anschliessend musst du auf dem Scoresheet unterschreiben um das Resultat zu bestätigen. Bevor du
+dies tust, vergewissere dich dass die Zeit richtig aufgeschrieben wurde. Wenn du unsicher bist,
+zögere auch hier nicht nachzufragen oder sogar den WCA Delegierten herbeizurufen.
 
-Wenn du noch weitere Versuche machen darfst, wird der Judge das Puzzle mitnehmen und zurück zum Scrambling Tisch bringen. Du musst dich dann zurück in den Wartebereich setzen. Ansonsten ist das Event für dich vorbei und du kannst dein Puzzle wieder mitnehmen.
+Wenn du noch weitere Versuche machen darfst, wird der Judge das Puzzle mitnehmen und zurück zum
+Scrambling Tisch bringen. Du musst dich dann zurück in den Wartebereich setzen. Ansonsten ist das
+Event für dich vorbei und du kannst dein Puzzle wieder mitnehmen.
 </p>
 
 <h2>Judging</h2>
 
 <p>
-Dieser erste Abschnitt des Tutorials diente dazu, dich auf ein Turnier als Teilnehmer vorzubereiten. Als Teilnehmer eines WCA Turniers ist man allerdings auch dazu verpflichtet, als Schiedsrichter (Judge) mitzuhelfen, wenn man selber gerade nicht teilnimmt. Schau dir hierzu  <a href="judging.php"><b>Teil 2:</b> Judging Tutorial</a> an.
+Dieser erste Abschnitt des Tutorials diente dazu, dich auf ein Turnier als Teilnehmer vorzubereiten.
+Als Teilnehmer eines WCA Turniers ist man allerdings auch dazu verpflichtet, als
+Schiedsrichter (Judge) mitzuhelfen, wenn man selber gerade nicht teilnimmt.
+Schau dir hierzu <a href="judging.php"><b>Teil 2:</b> Judging Tutorial</a> an.
 </p>
 
 <? include "footer.php" ?>
--- judging.php
+++ judging.php
@@ -17,76 +17,131 @@ von <a href="http://www.cubingusa.com">Cubing USA</a>. <br/>
 <? include "menu.php" ?>
 
 <p>
-Du bist Teilnehmer eines WCA Wettbewerbs. Dann ist es deine Pflicht, auch als Schiedsrichter ("Judge") tätig zu sein. Keine Angst, es ist im Grunde nicht schwer. Du solltest dazu die <a href="https://www.worldcubeassociation.org/regulations/translations/german/">WCA Regeln</a> einmal gelesen und verstanden haben, aber du musst sie nicht auswendig kennen.
+Du bist Teilnehmer eines WCA Wettbewerbs. Dann ist es deine Pflicht, auch als
+Schiedsrichter ("Judge") tätig zu sein. Keine Angst, es ist im Grunde nicht schwer. Du solltest
+dazu die
+<a href="https://www.worldcubeassociation.org/regulations/translations/german/">WCA Regeln</a>
+einmal gelesen und verstanden haben, aber du musst sie nicht auswendig kennen.
 </p>
 
 <p>
-Im Folgenden geben wir dir eine vereinfachte Zusammenfassung zum Thema Judging. Diese Hinweise beziehen sich in erster Linie
-auf das "normale" Judgen von Puzzeln wie dem 3x3x3 Würfel (für Spezialdisziplinen wie z.B. das Blindlösen gelten teilweise andere Regeln). Die Beschreibung ist darauf ausgelegt, die überwiegende Mehrheit an Versuchen abzudecken. Selten auftretende Spezialfälle lassen wir bewusst aus, um dich als Neuling nicht zu sehr abzuschrecken. Wenn auf einem Turnier doch einmal etwas passiert, was in dieser Beschreibung nicht ausführlich erklärt wurde, frage bitte andere Schiedsrichter oder den WCA Delegierten (nicht den Teilnehmer!). Habe keine Angst davor anderen auf die Nerven zu fallen! Der WCA Delegierte und die Organisatoren des Turniers sind immer froh über engagierte Helfer und sind dafür gerne bereit, Fragen zu beantworten. Stell lieber viele Fragen als einen Fehler zu machen, weil du dich nicht traust.
+Im Folgenden geben wir dir eine vereinfachte Zusammenfassung zum Thema Judging. Diese Hinweise
+beziehen sich in erster Linie auf das "normale" Judgen von Puzzeln wie dem 3x3x3 Würfel (für
+Spezialdisziplinen wie z.B. das Blindlösen gelten teilweise andere Regeln). Die Beschreibung ist
+darauf ausgelegt, die überwiegende Mehrheit an Versuchen abzudecken. Selten auftretende
+Spezialfälle lassen wir bewusst aus, um dich als Neuling nicht zu sehr abzuschrecken. Wenn auf
+einem Turnier doch einmal etwas passiert, was in dieser Beschreibung nicht ausführlich erklärt
+wurde, frage bitte andere Schiedsrichter oder den WCA Delegierten (nicht den Teilnehmer!). Habe
+keine Angst davor anderen auf die Nerven zu fallen! Der WCA Delegierte und die Organisatoren des
+Turniers sind immer froh über engagierte Helfer und sind dafür gerne bereit, Fragen zu
+beantworten. Stell lieber viele Fragen als einen Fehler zu machen, weil du dich nicht traust.
 </p>
 
 <h2>"+2" und "DNF"</h2>
 <p>
-In manchen Fällen kann es dazu kommen, dass ein Teilnehmer eine oder mehrere Zeitstrafen von zwei Sekunden erhält ("+2"). Wieder andere Situationen führen dazu, dass der Versuch eines Teilnehmers als ungültig gewertet wird ("DNF"). Wir markieren diese Situationen im Folgenden entsprechend. Wir erwarten von dir als Neuling aber nicht, dass du diese Entscheidungen selbstständig triffst. Vielmehr dienen diese Markierungen als Hinweis für dich, wann du wie oben erklärt, andere Personen um Hilfe bitten solltest.
+In manchen Fällen kann es dazu kommen, dass ein Teilnehmer eine oder mehrere Zeitstrafen von zwei
+Sekunden erhält ("+2"). Wieder andere Situationen führen dazu, dass der Versuch eines Teilnehmers
+als ungültig gewertet wird ("DNF"). Wir markieren diese Situationen im Folgenden entsprechend. Wir
+erwarten von dir als Neuling aber nicht, dass du diese Entscheidungen selbstständig triffst.
+Vielmehr dienen diese Markierungen als Hinweis für dich, wann du wie oben erklärt, andere Personen
+um Hilfe bitten solltest.
 </p>
 
 <h2>Wann soll ich judgen?</h2>
 
 <p>
-Du darfst in Gruppen, in denen du selber teilnimmst, kein Schiedsrichter sein (es sei denn, du hast bereits alle deine Versuche hinter dich gebracht). In allen anderen Gruppen darfst und sollst du judgen.
+Du darfst in Gruppen, in denen du selber teilnimmst, kein Schiedsrichter sein (es sei denn, du hast
+bereits alle deine Versuche hinter dich gebracht). In allen anderen Gruppen darfst und sollst du
+judgen.
 </p>
 
 <h2>Wen und wie judge ich?</h2>
 
 <p>
-Wen du judgen sollst ist nicht fest zugeteilt. Im Gegenteil: Nimm dir selbstständig ein gemischtes Puzzle mit Abdeckung und dem dazugehörigen Scoresheet vom Scramblingtisch und gehe damit zum Wartebereich. Lies laut und deutlich den Namen des Teilnehmers vom Scoresheet vor und gehe mit ihm zu einem freien Timertisch, sobald er sich meldet.
+Wen du judgen sollst ist nicht fest zugeteilt. Im Gegenteil: Nimm dir selbstständig ein gemischtes
+Puzzle mit Abdeckung und dem dazugehörigen Scoresheet vom Scramblingtisch und gehe damit zum
+Wartebereich. Lies laut und deutlich den Namen des Teilnehmers vom Scoresheet vor und gehe mit
+ihm zu einem freien Timertisch, sobald er sich meldet.
 </p>
 
 <h2>Halte den Würfel verdeckt</h2>
 
 <p>
-Die Puzzle sind am Anfang verdeckt. Entweder mit einem Plastikdeckel oder einem Blatt Papier. Es ist sehr wichtig, dass der Teilnehmer das Puzzle nicht sehen kann, also achte sehr gut darauf.
+Die Puzzle sind am Anfang verdeckt. Entweder mit einem Plastikdeckel oder einem Blatt Papier. Es
+ist sehr wichtig, dass der Teilnehmer das Puzzle nicht sehen kann, also achte sehr gut darauf.
 </p>
 
 <h2>Sorge dafür, dass alles bereit ist</h2>
 
 <p>
-Sorge dafür, dass auf dem Tisch nichts im Weg liegt. Schalte den Stackmat Timer ein und lösche gegebenenfalls die Zeit, so dass der Timer auf "0.00" steht ("Reset"-Knopf). Sorge auch dafür, dass die bereitliegende Stoppuhr ebenfalls auf "0.00" steht.
+Sorge dafür, dass auf dem Tisch nichts im Weg liegt. Schalte den Stackmat Timer ein und lösche
+gegebenenfalls die Zeit, so dass der Timer auf "0.00" steht ("Reset"-Knopf). Sorge auch dafür,
+dass die bereitliegende Stoppuhr ebenfalls auf "0.00" steht.
 </p>
 
 <h2>Frage "Bereit?"</h2>
 
 <p>
-Frage den Teilnehmer, ob er bereit ist ("Ready?"). Sobald er dies klar bejaht ("Ja!" oder "Yes!"), nimm die Abdeckung herunter und starte gleichzeitig die Stoppuhr, womit die Inspection Phase beginnt.
+Frage den Teilnehmer, ob er bereit ist ("Ready?"). Sobald er dies klar bejaht ("Ja!" oder
+"Yes!"), nimm die Abdeckung herunter und starte gleichzeitig die Stoppuhr, womit die Inspection
+Phase beginnt.
 </p>
 
 <h2>Inspection Phase</h2>
 
 <p>
-Sobald die Stoppuhr 8 Sekunden zeigt, sage laut und deutlich "8 seconds". Sobald die Stoppuhr 12 Sekunden zeigt, sage laut und deutlich "12 seconds". Sobald der Teilnehmer den Timer gestartet hat, stoppst du die Stoppuhr. Fast immer geschieht dies vor Ablauf von 15 Sekunden. Sollte die Stoppuhr eine höhere Zeit anzeigen, rufe nach Ende des Versuchs den WCA Delegierten zur Hilfe.
+Sobald die Stoppuhr 8 Sekunden zeigt, sage laut und deutlich "8 seconds". Sobald die Stoppuhr
+12 Sekunden zeigt, sage laut und deutlich "12 seconds". Sobald der Teilnehmer den Timer gestartet
+hat, stoppst du die Stoppuhr. Fast immer geschieht dies vor Ablauf von 15 Sekunden. Sollte die
+Stoppuhr eine höhere Zeit anzeigen, rufe nach Ende des Versuchs den WCA Delegierten zur Hilfe.
 </p>
 
 <h2>Während der Teilnehmer löst</h2>
 
 <p>
-Während der Teilnehmer löst, solltest du nichts tun, was ihn ablenken könnte, z.B. selber mit einem Puzzle herumspielen oder dich mit anderen Personen unterhalten. Insbesondere solltest du ihn als Schiedsrichter aufmerksam beobachten. Stell zum Beispiel sicher, dass der Timer nicht vorzeitig gestoppt wird ("DNF") und die gesamte Zeit bis zum Ende korrekt läuft. Fast immer läuft alles ohne Zwischenfälle ab, aber wenn doch einmal etwas besonderes geschieht, solltest du als Schiedsrichter dem WCA Delegierten davon berichten können.
+Während der Teilnehmer löst, solltest du nichts tun, was ihn ablenken könnte, z.B. selber mit
+einem Puzzle herumspielen oder dich mit anderen Personen unterhalten. Insbesondere solltest du
+ihn als Schiedsrichter aufmerksam beobachten. Stell zum Beispiel sicher, dass der Timer nicht
+vorzeitig gestoppt wird ("DNF") und die gesamte Zeit bis zum Ende korrekt läuft. Fast immer
+läuft alles ohne Zwischenfälle ab, aber wenn doch einmal etwas besonderes geschieht, solltest
+du als Schiedsrichter dem WCA Delegierten davon berichten können.
 </p>
 
 <h2>Ende des Versuchs</h2>
 
 <p>
-Der Teilnehmer sollte das Puzzle auf den Tisch legen und die Zeit stoppen. Der Timer muss mit den Handflächen gestoppt werden, nicht mit den Handkanten ("+2"). Der Teilnehmer darf das Puzzle in diesem Moment nicht mehr berühren ("+2"). Auch nachdem der Timer gestoppt wurde, darf der Teilnehmer das Puzzle erst wieder berühren, sobald du das Puzzle genau angeschaut hast und festgestellt hast, ob es korrekt gelöst ist ("+2" oder "DNF"). Achtung: Mache dies ohne das Puzzle in die Hand zu nehmen!  Ist das Puzzle eindeutig gelöst, sagst du "OK" und das Puzzle ist hiermit für den Teilnehmer zum Anfassen freigegeben. Sind allerdings eine oder mehrere Seiten nicht korrekt ausgerichtet, kann dies "+2" oder "DNF" bedeuten (siehe <a href="https://www.worldcubeassociation.org/regulations/translations/german/#article-10-solved-state">Abschnitt 10</a>  der WCA Regeln). Frage wie immer im Zweifelsfall einen anderen Schiedsrichter oder den WCA Delegierten um Rat. Hole diese Personen dann zum Tisch und lass das Puzzle unberührt an seiner Stelle liegen!
+Der Teilnehmer sollte das Puzzle auf den Tisch legen und die Zeit stoppen. Der Timer muss mit den
+Handflächen gestoppt werden, nicht mit den Handkanten ("+2"). Der Teilnehmer darf das Puzzle in
+diesem Moment nicht mehr berühren ("+2"). Auch nachdem der Timer gestoppt wurde, darf der
+Teilnehmer das Puzzle erst wieder berühren, sobald du das Puzzle genau angeschaut hast und
+festgestellt hast, ob es korrekt gelöst ist ("+2" oder "DNF"). Achtung: Mache dies ohne das Puzzle
+in die Hand zu nehmen!  Ist das Puzzle eindeutig gelöst, sagst du "OK" und das Puzzle ist hiermit
+für den Teilnehmer zum Anfassen freigegeben. Sind allerdings eine oder mehrere Seiten nicht korrekt
+ausgerichtet, kann dies "+2" oder "DNF" bedeuten (siehe <a href="https://www.worldcubeassociation.org/regulations/translations/german/#article-10-solved-state">Abschnitt 10</a>
+der WCA Regeln). Frage wie immer im Zweifelsfall einen anderen Schiedsrichter oder den
+WCA Delegierten um Rat. Hole diese Personen dann zum Tisch und lass das Puzzle unberührt an seiner
+Stelle liegen!
 </p>
 
 <h2>Zeit aufschreiben</h2>
 
 <p>
-Falls der Teilnehmer keine Strafe erhält, schreibst du die Zeit genau so auf, wie der Timer sie anzeigt. Schreibe leserlich, damit die Zeiten später korrekt eingetragen werden können. Im Fall von "+2", schreibe die Zeit auf gefolgt von "+2" und der endgültigen Zeit (z.B. "17.65 + 2 = 19.65"). Im Fall von "DNF", schreibst du "DNF" auf. Anschließend unterschreibst du in dem dafür vorgesehenen Feld, damit man später weiß, dass du der Schiedsrichter des Versuchs warst. Nun lässt du den Teilnehmer unterschreiben - er bestätigt damit, dass du alles korrekt notiert hast.</p>
+Falls der Teilnehmer keine Strafe erhält, schreibst du die Zeit genau so auf, wie der Timer sie
+anzeigt. Schreibe leserlich, damit die Zeiten später korrekt eingetragen werden können. Im Fall
+von "+2", schreibe die Zeit auf gefolgt von "+2" und der endgültigen Zeit
+(z.B. "17.65 + 2 = 19.65"). Im Fall von "DNF", schreibst du "DNF" auf. Anschließend unterschreibst
+du in dem dafür vorgesehenen Feld, damit man später weiß, dass du der Schiedsrichter des Versuchs
+warst. Nun lässt du den Teilnehmer unterschreiben - er bestätigt damit, dass du alles korrekt
+notiert hast.</p>
 
 
 <h2>Nach dem Versuch</h2>
 <p>
-Falls der Teilnehmer alle Versuche erledigt hat, gib ihm sein Puzzle zurück und teile ihm mit, dass er fertig ist. In diesem Fall bringst du das fertige Scoresheet zum Computertisch, damit die Zeiten dort eingetragen werden können. Andernfalls bringst du das Puzzle mit dem Scoresheet zurück zum Scrambletisch, damit es für den nächsten Versuch gemischt werden kann. Hier nimmst du das nächste gemischte Puzzle und es geht von Neuem los!
+Falls der Teilnehmer alle Versuche erledigt hat, gib ihm sein Puzzle zurück und teile ihm mit, dass
+er fertig ist. In diesem Fall bringst du das fertige Scoresheet zum Computertisch, damit die Zeiten
+dort eingetragen werden können. Andernfalls bringst du das Puzzle mit dem Scoresheet zurück zum
+Scrambletisch, damit es für den nächsten Versuch gemischt werden kann. Hier nimmst du das nächste
+gemischte Puzzle und es geht von Neuem los!
 </p>
 
 <? include "footer.php" ?>
